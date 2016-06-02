602fd589894ccfc14a298b61677b3d84f0fe94cc
Alison <almcgreavy@gmail.com>
2016-06-02 12:33:40 +0100

    Tests for base cases

--- src/main/java/com/autotrader/StringCalculator/StringCalculator.java
+++ src/main/java/com/autotrader/StringCalculator/StringCalculator.java
@@ -3,6 +3,11 @@ package com.autotrader.StringCalculator;
 public class StringCalculator {
 
 	public int calculateResult(String input) {
+		
+		if(input == null || "".equals(input)){
+			return -1;
+		}
+		
 		return 3;
 	}
 
--- src/test/java/com/autotrader/StringCalculator/StringCalculatorTest.java
+++ src/test/java/com/autotrader/StringCalculator/StringCalculatorTest.java
@@ -1,15 +1,32 @@
 package com.autotrader.StringCalculator;
 
-import org.junit.Assert;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Before;
 import org.junit.Test;
 
 
 public class StringCalculatorTest {
 	
-	@Test
-	public void shouldCalculateSingleDigitResult() {
-		StringCalculator stringCalculator = new StringCalculator();
-		Assert.assertEquals(3, stringCalculator.calculateResult("3"));
+	StringCalculator stringCalculator;
+	
+	@Before
+	public void setUp(){
+		stringCalculator = new StringCalculator();
 	}
 	
+	@Test
+	public void shouldCalculateSingleDigitResult() {
+		assertThat(stringCalculator.calculateResult("3")).isEqualTo(3);
+	}
+	
+	@Test
+	public void shouldCalculateNullResult() {
+		assertThat(stringCalculator.calculateResult(null)).isEqualTo(-1);
+	}
+	
+	@Test
+	public void shouldCalculateEmptyResult(){
+		assertThat(stringCalculator.calculateResult("")).isEqualTo(-1);
+	}
 }
